e6b12c9115ca8a49e2b3c41a0845e7c49b14befb
Konstantin Honcharov <k.honcharov@gmail.com>
2022-02-10 22:06:48 +0300

    feat: add footer component

--- src/app/pages/components/footer.ts
+++ src/app/pages/components/footer.ts
@@ -1,3 +1,30 @@
+import rssLogo from '../../../assets/svg/logo-rss.svg';
+
 export const getFooter = (): string => {
-  return '<p>RSSchools 2022</p>';
+  return `
+    <footer class="footer">
+      <div class="footer__container">
+        <div class="footer__inner-container">
+          <a class="footer__link" href="https://rs.school/js/">
+            <img
+              class="footer__rss-logo"
+              src="${rssLogo}"
+              alt="Rolling Scopes School logo"
+            />
+          </a>
+          <div class="footer__links-container">
+            <a class="footer__link" href="https://github.com/WeronikaFed">
+              <i class="fab fa-github"></i> Вероника
+            </a>
+            <a class="footer__link" href="https://github.com/v3n9s">
+              <i class="fab fa-github"></i> Веньямин
+            </a>
+            <a class="footer__link" href="https://github.com/khoncharov">
+              <i class="fab fa-github"></i> Константин
+            </a>
+          </div>
+          <p class="footer__year">2022</p>
+        </div>
+      </div>
+    </footer>`;
 };
