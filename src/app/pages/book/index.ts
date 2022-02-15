0c39e064f15d02ef3634527795bceb06adaa29c2
weronika-fed <nikafed3@gmail.com>
2022-02-15 19:14:28 +0300

    feat: add book pages with words

--- src/app/pages/book/index.ts
+++ src/app/pages/book/index.ts
@@ -1,6 +1,8 @@
 import { updatePage } from '../../components/page';
 import { TPageComponent } from '../../router';
 import { BookParam, HashPath, IBookNav } from '../../types';
+import { IWord } from '../../api/IWord';
+import { getWords } from '../../api/get-words';
 
 function createBookMain(rootElement: HTMLDivElement) {
   const bookHeader = document.createElement('h1');
@@ -68,6 +70,81 @@ function createBookMain(rootElement: HTMLDivElement) {
   return rootElement;
 }
 
+function createWordElement(word: IWord): HTMLDivElement {
+  const wordCard = document.createElement('div');
+  wordCard.className = 'word-card';
+  const imageContainer = document.createElement('div');
+  const userChoose = document.createElement('div');
+  userChoose.innerHTML = `
+  <i class="far fa-star"></i>
+  <i class="far fa-check-circle"></i>
+  `;
+  const wordImg = document.createElement('img');
+  wordImg.src = `https://rs-school-learnwords.herokuapp.com/${word.image}`;
+
+  imageContainer.appendChild(userChoose);
+  imageContainer.appendChild(wordImg);
+
+  function wordSound() {
+    const audio = new Audio(); 
+    audio.src = `https://rs-school-learnwords.herokuapp.com/${word.audio}`; 
+    audio.autoplay = true;
+  }
+
+  const someWord = document.createElement('div');
+  someWord.innerHTML = `
+  <p>${word.word} - ${word.wordTranslate}</p>
+  <hr>
+  <i class="far fa-play-circle word-sound"></i>
+  <p>/ ${word.transcription} /</p>
+  <p>Использование:</p>
+  <ul>
+  <li>${word.textMeaning}</li>
+  <li>${word.textMeaningTranslate}</li>
+  </ul>
+  <p>Пример:</p>
+  <ul>
+  <li>${word.textExample}</li>
+  <li>${word.textExampleTranslate}</li>
+  </ul>
+  `;
+  const sound = someWord.querySelector('.word-sound');
+  sound!.addEventListener('click', wordSound);
+
+  wordCard.appendChild(imageContainer);
+  wordCard.appendChild(someWord);
+
+  return wordCard;
+}
+
+async function createBookGroup(group: number, page: number, rootElement: HTMLDivElement) {
+  const mainContent = document.createElement('div');
+  mainContent.className = 'main-content';
+
+  const navigationBlock = document.createElement('div');
+  navigationBlock.className = 'navigation-block';
+
+  const words = await getWords(group - 1, page - 1);
+
+  navigationBlock.innerHTML = `
+  <i class="far fa-arrow-alt-circle-up"></i>
+  <p>Раздел ${group} / Страница ${page}</p>
+  <i class="far fa-arrow-alt-circle-left"></i>
+  <i class="far fa-arrow-alt-circle-right"></i>
+  `;
+
+  const wordsContainer = document.createElement('div');
+
+  for (let i = 0; i <= 19; i += 1) {
+    let word = createWordElement(words[i]);
+    wordsContainer.appendChild(word);
+  }
+
+  mainContent.appendChild(navigationBlock);
+  mainContent.appendChild(wordsContainer);
+  rootElement.appendChild(mainContent);
+}
+
 const pageContent = (params: IBookNav): HTMLElement => {
   const node = document.createElement('div');
   node.className = 'main__container';
@@ -80,7 +157,7 @@ const pageContent = (params: IBookNav): HTMLElement => {
     createBookMain(node);
     console.log('Book loaded');
   } else {
-    // запускаем book на group - page
+    createBookGroup(group, page, node);
     console.log('Book loaded on G:', group, ', P: ', page);
   }
   
