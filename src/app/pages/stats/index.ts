31aaa829eb8931a9585d2e9c173e30cf364b81cb
Konstantin Honcharov <k.honcharov@gmail.com>
2022-02-19 11:20:14 +0300

    feat: initail styling

--- src/app/pages/stats/index.ts
+++ src/app/pages/stats/index.ts
@@ -1,17 +1,25 @@
 import { TPageComponent } from '../../router';
 import { updatePage } from '../../components/page';
+import { store } from '../../redux/store';
 
 const pageContent = (): HTMLElement => {
   const node = document.createElement('div');
   node.className = 'main__container';
 
+  const userName = store.getState().user.name ?? '';
+
   node.innerHTML = `
-    <br>
-    <br>
-    <h1>Статистика</h1>
-    <p>123 ...</p>
-    <br>
-    <br>`;
+    <section class="home">
+      <div class="home__header-container">
+        <h2 class="home__caption">Статистика пользователя <b>${userName}</b></h2>
+      </div>
+      <div class="stats__content-container">
+        <div class="stats__not-auth">
+          <p>Для просмотра и накопления статистики необходимо
+          авторизоваться <i class="fa-regular fa-face-rolling-eyes"></i></p>
+        </div>
+      </div>
+    </section>`;
 
   return node;
 };
