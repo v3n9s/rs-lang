1dad0490ca31edad449a05654e673018eb103a79
Konstantin Honcharov <k.honcharov@gmail.com>
2022-02-21 06:46:23 +0300

    feat: add unauth stats page content

--- src/app/pages/stats/index.ts
+++ src/app/pages/stats/index.ts
@@ -2,24 +2,47 @@ import { TPageComponent } from '../../router';
 import { updatePage } from '../../components/page';
 import { store } from '../../redux/store';
 
+function unauthContent(): string {
+  return `
+    <section class="home">
+      <div class="home__header-container">
+        <h2 class="home__caption">Статистика пользователя</h2>
+      </div>
+      <div class="stats__content-container">
+        <div class="stats__not-auth">
+          <p class="stats__text">Для накопления и просмотра статистики необходимо
+          авторизоваться <i class="fa-regular fa-face-rolling-eyes"></i></p>
+        </div>
+      </div>
+    </section>`;
+}
+
+function authContent(): string {
+  const userName = store.getState().user.name;
+  return `
+    <section class="home">
+      <div class="home__header-container">
+        <h2 class="home__caption">Статистика пользователя <b>${userName}</b></h2>
+      </div>
+      <div class="stats__content-container">
+        <div class="">
+          
+        </div>
+      </div>
+    </section>`;
+}
+
 const pageContent = (): HTMLElement => {
   const node = document.createElement('div');
   node.className = 'main__container';
 
   const userName = store.getState().user.name ?? '';
 
-  node.innerHTML = `
-    <section class="home">
-      <div class="home__header-container">
-        <h2 class="home__caption">Статистика пользователя <b>${userName}</b></h2>
-      </div>
-      <div class="stats__content-container">
-        <div class="stats__not-auth">
-          <p>Для просмотра и накопления статистики необходимо
-          авторизоваться <i class="fa-regular fa-face-rolling-eyes"></i></p>
-        </div>
-      </div>
-    </section>`;
+  if (userName) {
+    node.innerHTML = authContent();
+  } else {
+    node.innerHTML = unauthContent();
+  }
 
   return node;
 };
